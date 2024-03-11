930136331bfd33fe9e3a6a61fadda5e51027c505
Dobromir Marinov <mr.d.marinov@gmail.com>
2024-03-11 03:55:02 +0000

    * Updated attack script.

--- run_attacks.sh
+++ run_attacks.sh
@@ -8,7 +8,7 @@ declare -a attack=(
 'RandomAttackOnline(attack_dataset="train",rate=0.3,batch_size=200)'
 'ConstrainedAttack(attack_dataset="train",rate=0.3,kde_bandwidth=0.1,time_window=100,max_node_degree_strat="median")'
 'ConstrainedAttackOnline(attack_dataset="train",rate=0.3,batch_size=200,kde_bandwidth=0.1,time_window=100,max_node_degree_strat="median")'
-'EvolutionaryAttack(attack_dataset="train,validation",rate=0.3,mutation_rate=0.25,mutation_range=100)'
+'EvolutionaryAttack(attack_dataset="train",rate=0.3,mutation_rate=0.25,mutation_range=100)'
 'EvolutionaryAttackOnline(attack_dataset="train",rate=0.3,batch_size=200,mutation_rate=0.25,mutation_range=100)'
 )
 
@@ -19,5 +19,5 @@ done
 
 for a in "${attack[@]}"
 do
-   python examples/linkproppred/tgbl-coin/dyrep.py --seed 1 --reduce_ratio 0.01 --attack $a
+   python examples/linkproppred/tgbl-coin/dyrep.py --seed 1 --reduce_ratio 0.1 --attack $a
 done
